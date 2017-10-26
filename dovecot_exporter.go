d8af6ad8302f647dbd3c18a03ce78011adcb3804
Jasper Spaans <jasper@startmail.com>
2017-10-26 15:05:33 +0200

    Make scopes to query configurable.

--- dovecot_exporter.go
+++ dovecot_exporter.go
@@ -34,7 +34,7 @@ var (
 		"Whether scraping Dovecot's metrics was successful.",
 		[]string{"scope"},
 		nil)
-	dovecotScopes = [...]string{"user"}
+	dovecotScopes []string
 )
 
 // Converts the output of Dovecot's EXPORT command to metrics.
@@ -139,9 +139,11 @@ func main() {
 		listenAddress = flag.String("web.listen-address", ":9166", "Address to listen on for web interface and telemetry.")
 		metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 		socketPath    = flag.String("dovecot.socket-path", "/var/run/dovecot/stats", "Path under which to expose metrics.")
+		scopesStr     = flag.String("dovecot.scopes", "user", "List of stats scopes to query (comma separated)")
 	)
 	flag.Parse()
 
+	dovecotScopes = strings.Split(*scopesStr, ",")
 	exporter := NewDovecotExporter(*socketPath)
 	prometheus.MustRegister(exporter)
 
